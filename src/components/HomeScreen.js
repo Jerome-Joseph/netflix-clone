375bdceb12b3fdfe0da3bfb777bedae132fbec40
Jerome-Joseph <jeromejoseph782@gmail.com>
2022-03-21 23:51:32 +0530

    commit 1

---
+++ src/components/HomeScreen.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Nav from './Nav'
+import "./styles/HomeScreen.css"
+
+const Homescreen = () => {
+  return (
+    <div>
+        <Nav />
+    </div>
+  )
+}
+
+export default Homescreen
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC0uzsOCRboVJqRMZvBJsiZW14wV1hM0D4",
+    authDomain: "netflix-clone-d57f4.firebaseapp.com",
+    projectId: "netflix-clone-d57f4",
+    storageBucket: "netflix-clone-d57f4.appspot.com",
+    messagingSenderId: "995707973735",
+    appId: "1:995707973735:web:6b42d6362eba8e581ee4c2"
+  };
