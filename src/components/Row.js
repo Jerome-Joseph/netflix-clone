e26de5e3809a70bd897b43caf2d087fa751c84b5
Jerome-Joseph <jeromejoseph782@gmail.com>
2022-03-24 20:56:43 +0530

    commit 4

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,10 +1,13 @@
 import axios from './axios'
 import React, { useEffect, useState } from 'react'
 import "./styles/Row.css"
+import YouTube from 'react-youtube'
+import movieTrailer from 'movie-trailer'
 
 const Row = ({title , fetchUrl , isLargeRow= false}) => {
 
     const [movies, setMovies] = useState([])
+    const [trailerUrl, setTrailerUrl] = useState("");
 
     const base_url = "https://image.tmdb.org/t/p/original/";
 
@@ -18,7 +21,26 @@ const Row = ({title , fetchUrl , isLargeRow= false}) => {
         fetchData();
     }, [fetchUrl]);
 
-    console.log(movies);
+    const opts={
+        height:"390",
+        width:"100%",
+        playerVars:{
+            autoplay:1,
+        },
+    };
+
+    const handleClick = (movie) =>{
+        if(trailerUrl){
+            setTrailerUrl('');
+        }
+        else{
+            movieTrailer(movie?.name || "").then(url =>{
+                const urlParams = new URLSearchParams(new URL(url).search);
+                setTrailerUrl(urlParams.get("v"));
+            })
+            .catch((error) => console.log(error));
+        }
+    }
     
   return (
     <div className='row'>
@@ -28,6 +50,7 @@ const Row = ({title , fetchUrl , isLargeRow= false}) => {
                 (isLargeRow && movie.poster_path) ||
                 (!isLargeRow && movie.backdrop_path)) && (
                     <img
+                    onClick={()=> handleClick(movie)}
                     className={`row__poster ${isLargeRow && "row__posterLarge"}`}
                     src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path
                     }`}
@@ -36,6 +59,7 @@ const Row = ({title , fetchUrl , isLargeRow= false}) => {
                     />
             ))}
         </div>
+        {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
     </div>
   )
 }
